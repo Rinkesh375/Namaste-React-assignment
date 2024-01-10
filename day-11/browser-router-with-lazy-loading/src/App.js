88057fcab90c7aeee419f13d4d8d415b6d7bb06d
Rinkesh375 <rinkeshujjwal16@gmail.com>
2024-01-10 22:45:16 +0530

    day-11 live class activity done

---
+++ day-11/browser-router-with-lazy-loading/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { AllRoute } from './MainRoutes/AllRoute';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  return (
+  <>
+  <Header/>
+  <AllRoute/>
+  <Footer/>
+  </>
+  );
+}
+
+export default App;
